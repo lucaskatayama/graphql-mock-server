8331a77c2e4d1d5f0f97568f276f5b617da4e2cf
Lucas Katayama <lucas.katayama@zup.com.br>
2019-02-18 16:41:03 -0300

    no cors

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require('cors');
+
 
 var marked = require("marked");
 var highlightjs = require("highlight.js");
@@ -35,11 +37,12 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use("/public", express.static(path.join(__dirname, "public")));
-
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 const { schema, resolver } = glue("graphql");
 const executableSchema = makeExecutableSchema({
